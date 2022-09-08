e49787853182d31c1ce4868a1b4b3197fe25b56f
Gustavo Araujo <gusttavo212@gmail.com>
2022-09-08 11:13:52 -0300

    Remove non used tests and create test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    use WithFaker;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown() : void
+    {
+        parent::tearDown();
+    }
 }
