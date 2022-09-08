1247d8b8aca147697478ec43f965faa94fc2f4b3
Gustavo Araujo <gusttavo212@gmail.com>
2022-09-08 11:13:31 -0300

    Create action to make prize

---
+++ app/Actions/MakePrize.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Prize;
+use App\Models\Ticket;
+
+class MakePrize
+{
+    public static function execute(Prize $prize): void
+    {
+        Ticket::whereDate(
+            'created_at',
+            '<=',
+            $prize->created_at->subSecond(30)
+        )->each(function ($value) use ($prize) {
+            if (collect($value->numbers)->diffAssoc($prize->numbers)->isEmpty()) {
+                $value->update(['winner' => true]);
+                $prize->tickets()->save($value);
+            }
+        });
+    }
+}
